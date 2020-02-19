7eba9ecad0c8791f950f39e811cd21bddf1d4006
Tikondra <tikondra47@gmail.com>
2020-02-19 12:12:22 +0400

    save

--- source/js/script.js
+++ source/js/script.js
@@ -35,19 +35,11 @@ if (document.querySelector('.recommend__list')) {
   }
 }
 
-var catalogMenu = document.querySelector('.menu');
-var menuHeight;
-if (catalogMenu) {
-  menuHeight = catalogMenu.clientHeight;
-} else {
-  menuHeight = 120;
-}
-
 $("a.scroll-to").on("click", function(e){
   e.preventDefault();
   var anchor = $(this).attr('href');
   $('html, body').stop().animate({
-      scrollTop: $(anchor).offset().top - (menuHeight + 10)
+      scrollTop: $(anchor).offset().top - 120
   }, 800);
   var elements = document.querySelectorAll('.menu__link');
 
@@ -58,3 +50,50 @@ $("a.scroll-to").on("click", function(e){
   }
   $(this).addClass('menu__link--active');
 });
+
+$(document).ready(function(){
+  $('.form__input--js').inputmask("+7 (999) 999-99-99");
+});
+
+function validationForm () {
+  var form = document.querySelector('.form');
+  var phone = form.querySelector('.form__input--js');
+  var submitBtn = form.querySelector('.form__btn');
+
+  var postForm = function () {
+    form.reset();
+    creatSuccess();
+  };
+  var creatSuccess = function () {
+    submitBtn.textContent = 'Сообщение отправлено';
+    setTimeout(function () {
+      submitBtn.textContent = 'Отправить'
+    }, 2700);
+  };
+  var save = function (data) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'call.php');
+    xhr.send(data);
+  };
+
+  var onValidationForm = function () {
+    var regExp = /^\+7\s\(\d{3}\)\s\d{3}\-\d{2}\-\d{2}$/;
+
+    var tel = phone.value;
+    var valid = regExp.test(tel);
+    if (!valid) {
+      phone.setCustomValidity('Некоректный номер');
+    } else {
+      phone.setCustomValidity('');
+    }
+  };
+  phone.addEventListener('blur', onValidationForm)
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    save(new FormData(form));
+    postForm();
+  });
+}
+if (document.querySelector('.form')) {
+  validationForm();
+}
